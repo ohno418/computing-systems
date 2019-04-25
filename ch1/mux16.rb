b93da0ec071769654aabd1b9af7f31b91571afed
yutarofire <yutaro.ono.418@gmail.com>
2019-04-25 20:00:16 +0900

    16bit multiplexor

---
+++ ch1/mux16.rb
@@ -0,0 +1,15 @@
+require './mux'
+
+class Mux16
+  attr_accessor :a, :b, :sel
+
+  def initialize(a, b, sel)
+    self.a = a[0..15]
+    self.b = b[0..15]
+    self.sel = sel[0..15]
+  end
+
+  def out
+    (0..15).map { |i| Mux.new(a[i], b[i], sel[i]).out }
+  end
+end
