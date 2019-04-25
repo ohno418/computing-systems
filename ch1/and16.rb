70ca4a2374ce623dcf2fa922b340fcbbffbd3c01
yutarofire <yutaro.ono.418@gmail.com>
2019-04-25 19:55:06 +0900

    16bit logistic gates

---
+++ ch1/and16.rb
@@ -0,0 +1,14 @@
+require './and'
+
+class And16
+  attr_accessor :a, :b
+
+  def initialize(a, b)
+    self.a = a[0..15]
+    self.b = b[0..15]
+  end
+
+  def out
+    (0..15).map { |i| And.new(a[i], b[i]).out }
+  end
+end
---
+++ ch1/test/not16.rb
@@ -0,0 +1,8 @@
+require './not16'
+
+if Not16.new([0,1,0,0,0,1,0,1,1,0,0,1,1,0,0,0]).out == \
+             [1,0,1,1,1,0,1,0,0,1,1,0,0,1,1,1]
+  puts 'Success'
+else
+  raise 'Failed'
+end
